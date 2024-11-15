8b5e89ab96578a22ccaf6b1b9fdbd1f5502543e5
Caterina Serra <cserra@a.iescanpeublanc.cat>
2024-11-15 09:28:06 +0100

    empiezo views

--- picts/views.py
+++ picts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Fotos
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/Fotos_list.html', {})
